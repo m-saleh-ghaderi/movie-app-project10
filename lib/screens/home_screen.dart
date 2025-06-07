f86433352773a10655fbab669e3eb285fcf20547
saleh ghaderi <s.ghaderi@Jahad.local>
2025-06-07 09:51:35 +0330

    Initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+            child: Center(
+          child: Text('project10'),
+        )),
+      ),
+    );
+  }
+}
