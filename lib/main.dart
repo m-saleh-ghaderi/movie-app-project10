4e6bf7f8c5c42eb897d2bdb7b5abc0e5165a4cd2
m-saleh-ghaderi <it.ghaderii@gmail.com>
2025-06-12 16:01:47 +0330

    Added home screen and updated platform files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project10/screens/home_screen.dart';
+import 'package:project10/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
