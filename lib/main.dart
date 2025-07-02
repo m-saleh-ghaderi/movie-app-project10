ffebc7c2abc1c6d786004ec42fe002fbae55c568
m-saleh-ghaderi <it.ghaderii@gmail.com>
2025-07-02 10:23:17 +0330

    splash screen finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project10/screens/home/home_screen.dart';
+import 'package:project10/screens/splash/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return HomeScreen();
+    return MaterialApp(home: SplashScreen());
   }
 }
---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Stack(children: [
+          Image.asset(
+            'assets/logo.png',
+            width: 150, // You can adjust the size as needed
+            height: 150,
+          ),
+          Positioned(
+              left: 10,
+              top: 7,
+              child: Image.asset(
+                'assets/logo.png',
+                width: 150, // You can adjust the size as needed
+                height: 150,
+              ))
+        ]),
+      ),
+    );
+  }
+}
